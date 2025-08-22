20e6c11f036a08d4769bb8b976402f5687fc6e2c
DANNA SAGASTUME <dsagastumer@miumg.edu.gt>
2025-08-22 02:37:50 -0600

    Se cambiaron los datos de prueba

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -3,6 +3,6 @@ import { sum } from "../src/sum.js";
 
 describe("sum function", () => {
   it("should add two numbers", () => {
-    expect(sum(2, 3)).toBe(5);
+    expect(sum(3, 3)).toBe(6);
   });
 });
